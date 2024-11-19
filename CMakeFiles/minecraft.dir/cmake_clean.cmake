80dbc04454f6053b18f9c779228c3359ddd0a1c0
evankelch <evan.c.kelch@gmail.com>
2024-11-19 10:38:39 -0500

    hello world init

---
+++ CMakeFiles/minecraft.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minecraft.dir/src/main.c.o"
+  "CMakeFiles/minecraft.dir/src/main.c.o.d"
+  "bin/minecraft"
+  "bin/minecraft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/minecraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
