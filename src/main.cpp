bc31f2d163c5529c11bb46d15b32e1fb0f1a9b7f
evankelch <evan.c.kelch@gmail.com>
2024-12-02 12:37:25 -0500

    create swapchain views

--- src/main.cpp
+++ src/main.cpp
@@ -81,6 +81,7 @@ class HelloTriangleApplication {
         VkFormat mSwapFormat{};
         VkSwapchainKHR mSwapChain = VK_NULL_HANDLE;
         std::vector<VkImage> swapChainImages;
+        std::vector<VkImageView> swapChainViews;
 
         const std::vector<const char*> requiredDeviceExtensions = {
             "VK_KHR_portability_subset",
@@ -129,6 +130,7 @@ class HelloTriangleApplication {
             pickPhysicalDevice();
             createLogicalDevice();
             createSwapChain();
+            createSwapChainViews();
         }
 
         void createSurface() {
@@ -407,6 +409,32 @@ class HelloTriangleApplication {
             mSwapFormat = surfaceFormat.format;
         }
 
+        void createSwapChainViews() {
+            swapChainViews.resize(swapChainImages.size());
+            for (int i = 0; i < swapChainImages.size(); i ++) {
+                VkImageViewCreateInfo createInfo = {};
+                createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+                createInfo.image = swapChainImages[i];
+                createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+                createInfo.format = mSwapFormat;
+
+                createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+                createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+                createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+                createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+                createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+                createInfo.subresourceRange.baseArrayLayer = 0;
+                createInfo.subresourceRange.baseMipLevel = 0;
+                createInfo.subresourceRange.layerCount = 1;
+                createInfo.subresourceRange.levelCount = 1;
+
+                if (vkCreateImageView(mLogicalDevice, &createInfo, nullptr, &swapChainViews[i]) != VK_SUCCESS) {
+                    throw std::runtime_error("Failed to reate swap chain image view");
+                };
+            }
+        }
+
         void mainLoop() {
             SDL_Event e;
             bool quit = false;
@@ -420,6 +448,9 @@ class HelloTriangleApplication {
             }
         }
         void cleanup() {
+            for (VkImageView iView : swapChainViews) {
+                vkDestroyImageView(mLogicalDevice, iView, nullptr);
+            }
             vkDestroySwapchainKHR(mLogicalDevice, mSwapChain, nullptr);
             vkDestroyDevice(mLogicalDevice, nullptr);
             vkDestroySurfaceKHR(gInstance, mSurface, nullptr);
